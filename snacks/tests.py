b31af9830bf20ffc3a9d7b7c5621afa57f8e197e
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-06-14 19:26:56 +0300

    add test

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class Snacks_Test(SimpleTestCase):
+    def test_home_page_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    def test_home_page_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_about_page_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    def test_about_page_template(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,'about.html')
